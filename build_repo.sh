204adee5e09115508a784a63ed200e99d69eddfb
Amir Fahat <amirf@mit.edu>
2022-02-07 14:24:50 -0500

    Add script which cleans and builds jars from source code

---
+++ build_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Remove demo executables from the last build
+echo "Removing executables..."
+rm demo-apps/run/*.jar
+
+# Compile the repo without running any tests
+echo "Building source..."
+mvn --quiet clean install -DskipTests
+
+# Run tests for the packages that proxy uses
+echo "Running tests..."
+mvn --quiet surefire:test -pl demo-apps,californium-proxy2,californium-core 
