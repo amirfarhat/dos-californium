097e53153d7ac87956ee0953ff44e74b5456a42f
Ubuntu <ubuntu@coaper.csail.mit.edu>
2022-02-22 17:54:52 -0500

    Add interface fetching script

---
+++ deter/scripts/fetch_interface.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /proj/MIT-DoS/exp/coap-setup/deps/dos-californium/deter/scripts/config.sh
+
+# Record short hostname
+my_hostname=$(hostname | awk '{split($0, a, "."); print a[1]}')
+
+# Fetch the interface associated with the above hostname
+interface=$(\
+  grep "$my_hostname" $INTERFACES_FILE \
+  | awk '{ ORS="" } \
+         { split($0, a, " "); print a[2]; }'
+)
+
+# Output the interface without a newline at the end
+echo -n "$interface"
