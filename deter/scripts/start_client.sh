345d49ccd4703d956c026d201807d7a0f1e0620a
Ubuntu <ubuntu@coaper.csail.mit.edu>
2022-02-23 09:46:48 -0500

    Make client-specific oplog

--- deter/scripts/start_client.sh
+++ deter/scripts/start_client.sh
@@ -18,8 +18,21 @@ function log () {
 
 my_hostname=$(hostname | awk '{ ORS="" } {split($0, a, "."); print a[1]}')
 sudo mkdir -p $TMP_DATA
+
+# Create empty client ops log
 OPLOG="$TMP_DATA/${my_hostname}_ops.log"
 sudo touch $OPLOG
+sudo rm $OPLOG
+sudo touch $OPLOG
+
+# Create client ops log configuration
+OPLOG_CONF="$HOME/$my_hostname.oplog.conf"
+sudo touch $OPLOG_CONF
+sudo rm $OPLOG_CONF
+sudo touch $OPLOG_CONF
+echo "logfile $OPLOG"  >> $OPLOG_CONF
+echo "logfile flush 1" >> $OPLOG_CONF
+echo "log on"          >> $OPLOG_CONF
 
 log "Preparing client configuration..."
 bash $BIN_HOME/prepare_californium_configuration.sh $CLIENT_PROPERTIES_FILE $HOME/$CLIENT_PROPERTIES_FILE_NAME "${CLIENT_PROPERTIES[*]}" COAP.
@@ -27,8 +40,8 @@ log "Done!\n"
 
 if [[ $TCPDUMP -eq 1 ]]; then
   log "Running client tcpdump...\n"
-  sudo screen -c $UTILS_HOME/oplog.conf -d -m -L sudo $BIN_HOME/run_tcpdump.sh
+  sudo screen -c $OPLOG_CONF -d -m -L sudo $BIN_HOME/run_tcpdump.sh
 fi
 
 log "Running client...\n"
-sudo screen -c $UTILS_HOME/oplog.conf -d -m -L sudo $BIN_HOME/client_run.sh
+sudo screen -c $OPLOG_CONF -d -m -L sudo $BIN_HOME/client_run.sh
